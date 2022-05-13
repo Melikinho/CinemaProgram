590dd5ca91110f9ce5fb76497063c1ca92b92115
Melikinho <Jimmymelik10@gmail.com>
2022-05-13 15:43:05 +0200

    Recoding methods

--- CinemaProgram/Program.cs
+++ CinemaProgram/Program.cs
@@ -1,5 +1,9 @@
 ﻿class program
 {
+    static List<int> myList = new List<int>();
+    int price;
+    int age;
+
     static void Main(string[] args)
     {
         Console.WriteLine("*@*@* Hello, and welcome to the Cinema program. Please Navigate though the following command numbers *@*@* ");
@@ -7,38 +11,37 @@
         Console.WriteLine("0. for quit");
         Console.WriteLine("1. for price information");
         Console.WriteLine("2. calculate all persons which will attend. Total price will be given. ");
-        int option = Int32.Parse(Console.ReadLine());
-        int result = 0;
-        int age;
-        int person;
-        List<int> myList = new List<int>();
+        string option = (Console.ReadLine());
         // Räknar summan av alla bio biljettter // int total = myList.Sum();
 
-        do
+
         {
+            bool success = false;
 
             switch (option)
             {
-                case 0:
+                case "0":
                     Console.WriteLine("*Quitting the program*");
                     Environment.Exit(0);
                     break;
-                case 1:
+                case "1":
                     Console.WriteLine("Enter your age: ");
-                    Task.Delay(500);
-                    AddAge();
-                    break;
-
-                case 2:
-                    Console.WriteLine("you are at stage 2");
-                    break;
-                
-                case 3:
-                    Console.WriteLine("you are at stage 3");
+                    var age = int.Parse(Console.ReadLine());
+                    var price = AddAge(age);
                     
                     break;
-                
-                case 4:
+
+                case "2":
+                    Console.WriteLine("Printing program ");
+                    PrintOut10TimesString();
+                    break;
+
+                case "3":
+                    Console.WriteLine("you are at stage 3");
+
+                    break;
+
+                case "4":
                     Console.WriteLine("you are at stage 3");
 
                     break;
@@ -47,36 +50,60 @@
                     Console.WriteLine("Wrong command. Please re-enter your command");
                     break;
             }
-
+            while (!success) ;
         }
-        while (false);
+
     }
-    public static void AddAge() {
-        List<int> myList = new List<int>();
 
 
-        int age = int.Parse(Console.ReadLine());
+    public static int AddAge(int age)
+    {
         if (age < 20)
         {
             Console.WriteLine("You qualify for youth price. 80 SEK");
-            myList.Add(age);
+            return 80;
         }
-            if (age > 64)
-            {
-                Console.WriteLine("You qualify for pensioner price. 90 SEK");
-                myList.Add(age);
-            }
+        if (age > 64)
+        {
+            Console.WriteLine("You Qualify for pensioner price. 90 SEK");
+            return 90;
+        }
         else
         {
             Console.WriteLine("You qualify for Standard price. 120 SEK");
-            myList.Add(age);
+            return 120;
         }
     }
 
-    public void CalculateAge()
+
+    public static void PrintOut10TimesString()
     {
-        List<int> myList = new List<int>();
-        Console.WriteLine("Under Construction");
+        bool success = false;
+
+        do
+        {
+
+            Console.WriteLine("Enter your desired text to print it out 10 times: ");
+            string text = Console.ReadLine();
+
+            Console.WriteLine($"Enter amout of times to print : {text}");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+
+                for (int i = 1; i <= number; i++)
+                {
+                    Console.WriteLine($"input: {i}, {text}");
+
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("");
+            }
+        } while (success);
+
     }
+
 }
-    
+
